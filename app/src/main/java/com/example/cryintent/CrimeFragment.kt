358bbc7281460d43911376ad478dd34a761bae15
Frisel91 <goryach.1991@mail.ru>
2022-11-23 19:54:33 +0300

    Все сначала. 1 Модель данных Crime 2 Фрагмент CrimeFragment

---
+++ app/src/main/java/com/example/cryintent/CrimeFragment.kt
@@ -0,0 +1,25 @@
+package com.example.cryintent
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class CrimeFragment : Fragment() {
+
+    private lateinit var crime: Crime
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        crime = Crime()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_crime, container, false)
+    }
+}
