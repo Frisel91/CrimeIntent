dad7ecb6150fc382d9b6a2277c04a4d8cfb1fc72
Frisel91 <goryach.1991@mail.ru>
2022-11-26 17:20:48 +0300

    linearLayout to ConstraintLayout

--- app/src/main/java/com/example/cryintent/CrimeListFragment.kt
+++ app/src/main/java/com/example/cryintent/CrimeListFragment.kt
@@ -6,7 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -49,9 +51,31 @@ class CrimeListFragment : Fragment() {
         crimeRecyclerView.adapter = adapter
     }
 
-private inner class CrimeHolder(view: View): RecyclerView.ViewHolder(view){
-    val titleText: TextView = itemView.findViewById(R.id.crime_title)
+private inner class CrimeHolder(view: View): RecyclerView.ViewHolder(view), View.OnClickListener{
+    private lateinit var crime: Crime
+    val titleTextView: TextView = itemView.findViewById(R.id.crime_title)
     val dateTextView: TextView = itemView.findViewById(R.id.crime_date)
+    private val solvedImageView: ImageView = itemView.findViewById(R.id.crime_solved)
+
+    init {
+        itemView.setOnClickListener(this)
+    }
+
+    fun bind(crime: Crime){
+        this.crime = crime
+        titleTextView.text = this.crime.title
+        dateTextView.text = this.crime.date.toString()
+        solvedImageView.visibility = if (crime.isSolved) {
+            View.VISIBLE
+        }
+        else{
+            View.GONE
+        }
+    }
+
+    override fun onClick(v: View?) {
+        Toast.makeText(context, "${crime.title} pressed", Toast.LENGTH_SHORT ).show()
+    }
 }
     private inner class CrimeAdapter(var crimes: List<Crime>): RecyclerView.Adapter<CrimeHolder>(){
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CrimeHolder {
@@ -61,11 +85,9 @@ private inner class CrimeHolder(view: View): RecyclerView.ViewHolder(view){
 
         override fun onBindViewHolder(holder: CrimeHolder, position: Int) {
             val crime = crimes[position]
-            holder.apply {
-                titleText.text = crime.title
-                dateTextView.text = crime.date.toString()
+            holder.bind(crime)
             }
-        }
+
 
         override fun getItemCount() = crimes.size
 
